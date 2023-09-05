cc20eabd6c211fa6ba80eafbec1c9ca70ed9844a
Keith Hasvision Rogers <evarlv.n@gmail.com>
2023-09-05 11:44:05 -0400

    This is the shell of my email template first commit

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="x-apple-disable-message-reformatting">
+  <title>Email Template</title>
+  <!--[if mso]> 
+<noscript> 
+<xml> 
+<o:OfficeDocumentSettings> 
+<o:PixelsPerInch>96</o:PixelsPerInch> 
+</o:OfficeDocumentSettings> 
+</xml> 
+</noscript> 
+<![endif]-->
+<style>
+  table, td, div, h1, p{
+    font-family: Arial, sans-serif;
+  }
+  table, td{
+    border: 2px solid #000000 !important;
+  }
+</style>
+</head>
+<body style="margin:0; padding:0">
+  <table role="presentation" style="width:100%; border-collapse:collapse; border:0; border-spacing:0; background: #ffffff;">
+    <tr>
+      <td style="padding:0;" align="center">
+      <!-- Main Email Table -->
+        <table role="presentation" style="width:602px; border-collapse:collapse;border-spacing:0; border:1px solid #000000; text-align:left;">
+        <!-- Header -->
+          <tr>
+            <td style="padding:0; background:#ffffff" align="center">
+              <img src="./pics/Father_Daughter_Dance_Brooklyn_NY_0010 (1).jpg" style="height:auto; display:block;" width="500" alt="Father Daughter Dance">
+            </td>
+          </tr>
+          <!-- Content -->
+          <tr>
+            <td style="padding:46px 30px 32px 30px">
+              <table role="presentation"width="100%" style="border-collapse:0; border-spacing:0">
+                <tr>
+                  <td style="padding:0">
+                    <h1>NYC Event Photography</h1>
+                    <p>Capturing Memories to last forever</p>
+                    <p><a href="https://www.evalv.com">Website</a></p>
+                  </td>
+                </tr>
+                  <tr>
+                    <td style="padding:0">
+                      <table role="presentation" style="width:100%; border-collapse:0; border:0;border-spacing:0;">
+                        <tr>
+                          <td style="padding:0; width:260px; vertical-align:top;">
+                            <p><img src="./pics/Bride and Groom (3).jpg"width="260" style="height:auto; display:block;" alt="Wedding photography"></p>
+                            <p>Evarlv Photography crafts your wedding memories into timeless artistry. With a keen eye for emotions and details, we capture each heartfelt moment, preserving the essence of your special day. </p>
+                            <p><a href="https://www.evarlv.com">Click</a></p>
+                            </td>
+                          <td style="padding:0;line-height:0;font-size:0;">&nbsp;</td>
+                          <td style="padding:0; width:260px; vertical-align:top;">
+                            <p><img src="./pics/ben-wiens-Qcl98B8Bk3I-unsplash.jpg" width="260" style="height:auto; display:block" alt=""></p>
+                            <p>Change this photo and add text</p>
+                            <p><a href="https://www.evarlv.com">Check it out</a></p>
+                        
+                            </td>
+                        </tr>
+                    
+                      </table>
+                  
+                    </td>
+                  </tr>
+                
+                </table>
+            </td>
+          </tr>
+          <tr>
+            <td style="padding:35px; background:#ee4c50">
+              <table role="presentation" style="width:100%; border-collapse:collapse; border:0;border-spacing:0">
+                  <tr>
+                    <td styling="padding:0; width:50%" align="left" >
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum aut tenetur at iste animi, quas veniam illum temporibus consequuntur dolorum facere sapiente, esse quam pariatur fugiat tempora harum sit quaerat!</p>
+                    </td>
+                    <td styling="padding:0; width:50%" align="right">
+                      <table role="presentation" style="width:100%; border-collapse:collapse;border:0; border-spacing:0">
+                        <tr>
+                          <td style="padding:0">
+                            <p>
+                              <img style="background-color:cornflowerblue;" src="./pics/iconmonstr-facebook-6.svg" alt="facebook icon">
+                            </p>
+                            </td>
+                          <td style="padding:0;">
+                            <p>
+                              <img style="background-color:red;" src="./pics/iconmonstr-youtube-1.svg" alt="Youtube icon">
+                            </p>
+                            </td>
+                          <td style="padding:0">
+                            <p>
+                              <img style="background-color:coral" src="./pics/iconmonstr-instagram-12.svg" alt="Instagram icon">
+                            </p>
+                          </td>
+                        </tr>
+                    
+                      </table>
+                    </td>
+                  </tr>
+            
+                </table>
+          
+              </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+  
+</body>
+</html>
